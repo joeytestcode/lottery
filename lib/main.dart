dac85b6dc167802d7983c0cf3a47cb846caba888
joeyhwangGit <jjoeyhwang@gmail.com>
2022-12-11 00:10:55 +0900

    202212110010

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget getRow(List<int> numbers) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: numbers
           .map((e) => Text(
                 e.toString(),
